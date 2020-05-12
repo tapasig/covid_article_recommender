d87cedf82a5b3dccc27659aab33576c4da7625d0
celis <acelisalas@gmail.com>
2020-05-12 21:03:00 +0200

    add template

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask
+from utils import load_model
+from configuration import Configuration
+import json
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "top secret!"
+config = Configuration()
+model = load_model(config)
 
 
 @app.route("/", methods=["GET"])
 def index():
     return "Hello"
 
+
+@app.route("/recommendations/<id>", methods=["GET"])
+def recommendations(id):
+    if model.get(id):
+       return json.dumps(model.get(id))
 
 if __name__ == "__main__":
 
