99db34c525d1b19187e0e02068fb3ea071e3662a
tapas <tapasi03@gmail.com>
2020-05-16 16:30:56 +0530

    forgot to push the app.py file

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from utils import load_model
 from configuration import Configuration
 import json
-
+from recommender.forms import InputForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "top secret!"
+
 config = Configuration()
 model = load_model(config)
 
 
 @app.route("/", methods=["GET"])
-def index():
-    return "Hello"
 
+def index():
+    return render_template('index.html', title='Home')
+#    article = None
+#    form = InputForm(request.args)
+#    if form.validate():
+#    	article = request.args.get("article_id")
+#    return "Hello"
+#    return render_template('index.html', title='Home', form=form, article=article) 
+
+@app.route('/input')
+def input():
+    article = None
+    form = InputForm()
+    if form.validate():
+           article = request.args.get("article_id")
+    return render_template('input.html', title='Input', form=form, article=article)
 
 @app.route("/recommendations/<id>", methods=["GET"])
 def recommendations(id):
