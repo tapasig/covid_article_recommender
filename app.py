4661a65867ccf4735170015167dbd26c445a1731
tapas <tapasi03@gmail.com>
2020-06-14 15:04:23 +0530

    updated application to display recommendation for a given article

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, render_template, flash, redirect
+from flask import Flask, request, render_template, flash, redirect, url_for
 from utils import load_model
 from configuration import Configuration
 import json
 from recommender.forms import InputForm
+import pandas as pd
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "top secret!"
@@ -26,31 +27,49 @@ def index():
 #    return render_template('index.html', title='Home', form=form, article=article) 
 
 @app.route('/input', methods=["GET","POST"])
+
+
 def input():
     global model
     article = None
     recommendations = None
+    titles = {}
+    catalog = None
 
     form = InputForm()
-
     '''
     if form.validate_on_submit():
-        flash('Missing field for article_id={}'.format(form.article_id.data))
-        return redirect('/input')
+    	#to render the message update the base template
+        flash('Submit the paper_id{} of an article'.format(form.article_id.data))
+        return redirect(url_for('input'))
     else:
         recommendations = model.get(article)
     '''
     if form.validate():
     	article = request.form.get("article_id")
-    
 
     #model.get(article) returns the recommendations      
     if model.get(article):
+        #query = article
     	recommendations = model.get(article) #json.dump(model.get(article))
 
-    print("article=", article, "recommendations = ", type(recommendations))
+
+    #read the title from csv file
+    catalog = pd.read_csv('./data1/paper_catalog.csv')
+    print("read catalog ", type(catalog))
+#    print(type(article))
+
+    for reco in recommendations:
+        for index,row in catalog.itertuples(index=False):
+           if (reco == index):
+	        #print(article)
+                titles[reco] = row
+
+
+    print("article=", article, "title=", len(titles), "recommendations = ", len(recommendations))
     
-    return render_template('input.html', title='Input', form=form, article=article, recommendations=recommendations)
+
+    return render_template('input.html', titles=titles, form=form, article=article, recommendations=recommendations)
 
 @app.route("/recommendations/<id>", methods=["GET"])
 def recommendations(id):
@@ -60,4 +79,5 @@ def recommendations(id):
 if __name__ == "__main__":
 
     app.run(threaded=True, port=5000, debug=True)
+
 
