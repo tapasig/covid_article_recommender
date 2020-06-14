a10af1a1a793ff6a18500e5812e048bdc196e207
celis <acelisalas@gmail.com>
2020-06-14 16:15:25 +0200

    minor

--- app.py
+++ app.py
@@ -15,9 +15,11 @@ catalog = load_catalog(config)
 
 
 @app.route("/", methods=["GET"])
-#@app.route("/index", methods=["GET","POST"])
+# @app.route("/index", methods=["GET","POST"])
 def index():
-    return render_template('index.html', title='Home')
+    return render_template("index.html", title="Home")
+
+
 #    article = None
 #    form = InputForm(request.args)
 
@@ -26,54 +28,63 @@ def index():
 #    print("article = ", article)
 
 #    return "Hello"
-#    return render_template('index.html', title='Home', form=form, article=article) 
+#    return render_template('index.html', title='Home', form=form, article=article)
 
-@app.route('/input', methods=["GET","POST"])
+
+@app.route("/input", methods=["GET", "POST"])
 def input():
     article = None
     recommendations = None
     titles = {}  # type: Dict[id, title]
-    catalog = None
 
     form = InputForm()
-    '''
+    """
     if form.validate_on_submit():
         #to render the message update the base template
         flash('Submit the paper_id{} of an article'.format(form.article_id.data))
         return redirect(url_for('input'))
     else:
         recommendations = model.get(article)
-    '''
+    """
     if form.validate():
         article = request.form.get("article_id")
 
-    #model.get(article) returns the recommendations      
+    # model.get(article) returns the recommendations
     if model.get(article):
-        recommendations = model.get(article) #json.dump(model.get(article))
-        print("article=", article,  "recommendations = ", recommendations)
-        # TODO load_Catalog should return a dictionary instead of a dataframe
-        # TODO this loop is not efficient, by having the dictionary we will just look in the dict and thats better
-        if (recommendations != None):
+        recommendations = model.get(article)  # json.dump(model.get(article))
+        print("article=", article, "recommendations = ", recommendations)
+        if recommendations != None:
             print("recos are not empty")
             for reco in recommendations:
-                title = catalog.get(reco,"")
+                title = catalog.get(reco, "")
                 titles[reco] = title
         else:
             print("recos is empty")
-           
 
-    print("article=", article, "title=", len(titles), "recommendations = ", len(recommendations))
-    
+    print(
+        "article=",
+        article,
+        "title=",
+        len(titles),
+        "recommendations = ",
+        len(recommendations),
+    )
 
-    return render_template('input.html', titles=titles, form=form, article=article, recommendations=recommendations)
+    return render_template(
+        "input.html",
+        titles=titles,
+        form=form,
+        article=article,
+        recommendations=recommendations,
+    )
+
 
 @app.route("/recommendations/<id>", methods=["GET"])
 def recommendations(id):
     if model.get(id):
-       return json.dumps(model.get(id))
+        return json.dumps(model.get(id))
+
 
 if __name__ == "__main__":
 
     app.run(threaded=True, port=5000, debug=True)
-
-
