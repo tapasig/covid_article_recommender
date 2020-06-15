6add8f8ac43cd02f9e73d726f57dcfe783140e27
celis <acelisalas@gmail.com>
2020-06-15 06:25:55 +0200

    change allen ai to semanticscholar

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, render_template, flash, redirect, url_for
-from typing import Dict, Any
 from utils import load_model, load_catalog
 from configuration import Configuration
 import json
 from recommender.forms import InputForm
-import pandas as pd
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "top secret!"
@@ -15,45 +14,25 @@ catalog = load_catalog(config)
 
 
 @app.route("/", methods=["GET"])
-# @app.route("/index", methods=["GET","POST"])
 def index():
     return render_template("index.html", title="Home")
-
-
-#    article = None
-#    form = InputForm(request.args)
-
-#    if form.validate():
-#    	article = request.args.get("article_id")
-#    print("article = ", article)
-
-#    return "Hello"
-#    return render_template('index.html', title='Home', form=form, article=article)
 
 
 @app.route("/input", methods=["GET", "POST"])
 def input():
     article = None
     recommendations = None
-    titles = {}  # type: Dict[id, title]
+    titles = {}
 
     form = InputForm()
-    """
-    if form.validate_on_submit():
-        #to render the message update the base template
-        flash('Submit the paper_id{} of an article'.format(form.article_id.data))
-        return redirect(url_for('input'))
-    else:
-        recommendations = model.get(article)
-    """
+
+
     if form.validate():
         article = request.form.get("article_id")
         titles[article] = catalog.get(article, "")
 
-    # model.get(article) returns the recommendations
     if model.get(article):
-        recommendations = model.get(article)  # json.dump(model.get(article))
-        print("article=", article, "recommendations = ", recommendations)
+        recommendations = model.get(article)
         if recommendations != None:
             print("recos are not empty")
             for reco in recommendations:
@@ -61,15 +40,6 @@ def input():
                 titles[reco] = title
         else:
             print("recos is empty")
-
-    print(
-        "article=",
-        article,
-        "title=",
-        len(titles),
-        "recommendations = ",
-        recommendations
-    )
 
     return render_template(
         "input.html",
